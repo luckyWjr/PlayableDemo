559d35b8431ed6466644e7e94fdfde5d0e2c1d78
王江荣 <903036544@qq.com>
2021-06-15 21:32:04 +0800

    fix error: blend error when clip length different

--- Assets/AnimationPlayable/PlayBlendTreeAnimation.cs
+++ Assets/AnimationPlayable/PlayBlendTreeAnimation.cs
@@ -13,11 +13,12 @@ public class PlayBlendTreeAnimation : MonoBehaviour
 
     PlayableGraph m_graph;
     AnimationMixerPlayable m_mixerPlayable;
+    float m_walkClipLength, m_runClipLength;
 
     void Start()
     {
         m_graph = PlayableGraph.Create("ChanPlayableGraph");
-        //m_graph.SetTimeUpdateMode(DirectorUpdateMode.GameTime);
+        m_graph.SetTimeUpdateMode(DirectorUpdateMode.GameTime);
 
         var animationOutputPlayable = AnimationPlayableOutput.Create(m_graph, "AnimationOutput", GetComponent<Animator>());
         m_mixerPlayable = AnimationMixerPlayable.Create(m_graph, 2);
@@ -27,14 +28,19 @@ public class PlayBlendTreeAnimation : MonoBehaviour
         m_graph.Connect(runClipPlayable, 0, m_mixerPlayable, 1);
         animationOutputPlayable.SetSourcePlayable(m_mixerPlayable);
 
-        walkClipPlayable.SetSpeed(1.3f);
-        runClipPlayable.SetSpeed(0.8f);
+        m_walkClipLength = walkClip.length;
+        m_runClipLength = runClip.length;
+        walkClipPlayable.SetSpeed(m_walkClipLength);
+        runClipPlayable.SetSpeed(m_runClipLength);
         m_graph.Play();
     }
 
     void Update() {
         m_mixerPlayable.SetInputWeight(0, 1.0f - speed);
         m_mixerPlayable.SetInputWeight(1, speed);
+        //线性插值处理Blend效果不对的问题
+        float mixerPlayableSpeed = 1.0f / ((1.0f - speed) * m_walkClipLength + speed * m_runClipLength);
+        m_mixerPlayable.SetSpeed(mixerPlayableSpeed);
     }
 
     void OnDestroy() {
